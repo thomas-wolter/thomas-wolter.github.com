b670a61efb621f46825d8c64f81eaea81f1c52cd
Eric <eric.wolter@gmx.de>
2012-12-23 11:07:53 +0100

    removed rightclick on canvas

--- new/_src/contents/js/app.js
+++ new/_src/contents/js/app.js
@@ -14,24 +14,18 @@ $(document).ready(function() {
 	$('canvas').on(TouchMouseEvent.DOWN, function(ev){
 		ev.preventDefault();
 			
-		if(ev.originalEvent.which == 3) {
-			var canvas = $('canvas').get(0);
-			var dataUrl = canvas.toDataURL('image/png');
-			window.open(dataUrl, "toDataURL() image", "width=500, height=500");
-		} else {
-			var lastX = ev.pageX;
-			var lastY = ev.pageY;
+		var lastX = ev.pageX;
+		var lastY = ev.pageY;
+		
+		$('canvas').on(TouchMouseEvent.MOVE, function(ev) {
+			var diffX = ev.pageX - lastX;
+			var diffY = ev.pageY - lastY;
 			
-			$('canvas').on(TouchMouseEvent.MOVE, function(ev) {
-				var diffX = ev.pageX - lastX;
-				var diffY = ev.pageY - lastY;
-				
-				snake.rotateCamera(-diffY * Math.PI/180, -diffX * Math.PI/180, 0);
-				
-				lastX = ev.pageX;
-				lastY = ev.pageY;
-			});
-		}
+			snake.rotateCamera(-diffY * Math.PI/180, -diffX * Math.PI/180, 0);
+			
+			lastX = ev.pageX;
+			lastY = ev.pageY;
+		});
 		
 		return false;
 	});
